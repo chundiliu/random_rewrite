b13e0f7fe1d5895a4c02aa9493c2fc02f7be8de2
Chundi Liu <chundiliu@cs.toronto.edu>
2019-05-21 14:22:07 -0400

    64.3 ox_w_ransac

--- train_tensorflow.py
+++ train_tensorflow.py
@@ -19,7 +19,7 @@ import tensorflow as tf
 
 import os
 
-os.environ["CUDA_VISIBLE_DEVICES"]="2"
+os.environ["CUDA_VISIBLE_DEVICES"]="1"
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
 parser = argparse.ArgumentParser()
@@ -28,7 +28,7 @@ parser.add_argument('--seed', type=int, default=42, help='Random seed.')
 parser.add_argument('--epochs', type=int, default=99999, help='Number of epochs to train.')
 parser.add_argument('--hidden1', type=int, default=2048, help='Number of units in hidden layer 1.')
 parser.add_argument('--hidden2', type=int, default=512, help='Number of units in hidden layer 2.')
-parser.add_argument('--lr', type=float, default=0.0001, help='Initial learning rate.')
+parser.add_argument('--lr', type=float, default=0.0003, help='Initial learning rate.')
 parser.add_argument('--dropout', type=float, default=0.1, help='Dropout rate (1 - keep probability).')
 parser.add_argument('--dataset-str', type=str, default='cora', help='type of dataset.')
 parser.add_argument('--batch-size', type=int, default=4993, help='Batch size.')
@@ -67,12 +67,13 @@ def gae_for(args, aa):
     #X = np.concatenate((X.T,D.T)).T
     # load the distractor too, shape should be (2048, 1M)
 
-    #adj_Q_pos = np.load('adj_q_pos_ransac_gem_paris.npy')
-    #adj_pos = np.load('adj_pos_ransac_gem_paris.npy')
+    adj_Q_pos = np.load('/media/chundi/3b6b0f74-0ac7-42c7-b76b-00c65f5b3673/revisitop/cnnimageretrieval-pytorch/data/test/delf/roxford_query_ransac_delf_250.npy')
+    adj_pos = np.load('/media/chundi/3b6b0f74-0ac7-42c7-b76b-00c65f5b3673/revisitop/cnnimageretrieval-pytorch/data/test/delf/roxford_index_ransac_delf_250.npy')
 
-    adj, features = gen_graph_index(Q, X, k=4, k_qe=3, do_qe=False)  # -----> 5k
+    adj, features = gen_graph_index(adj_pos, Q, X, k=5, k_qe=3, do_qe=False)  # -----> 5k
 
-    adj_Q, features_Q = gen_graph(Q, X, k=5, k_qe=3, do_qe=False)
+    adj_Q, features_Q = gen_graph(adj_Q_pos, Q, X, k=10, k_qe=3,
+                                  do_qe=False)
 
     #adj, features = gen_graph_index(Q, X, k=5, k_qe=3, do_qe=False) #-----> 5k
 
@@ -110,13 +111,6 @@ def gae_for(args, aa):
     # Some preprocessing
     print("preprocessing adj")
     adj_norm = preprocess_graph(adj)
-    #adj_norm_label = preprocess_graph_sp(adj)
-
-    adj_label = adj_train + sp.eye(adj_train.shape[0]) #adj_norm_label + sp.eye(adj_train.shape[0]) #adj_train + sp.eye(adj_train.shape[0])
-    #rows, columns = adj_label.nonzero()
-    #adj_label[columns, rows] = adj_label[rows, columns]
-    # adj_label = sparse_to_tuple(adj_label)
-    #adj_label = torch.FloatTensor(adj_label.toarray())
 
     print("adj sum: " + str(adj.sum()))
     pos_weight = float(float(adj.shape[0]) * adj.shape[0] - adj.sum()) / adj.sum()
@@ -126,9 +120,9 @@ def gae_for(args, aa):
 
 
 
-    features_pre = (features)
+    features_pre = (adj_norm * features)
     #features_pre = features_pre / np.linalg.norm(features_pre, ord=2, axis=1).reshape((features_pre.shape[0], 1))
-    features_all_pre = (features_all)
+    features_all_pre = (adj_all_norm * features_all)
     #features_all_pre = features_all_pre / np.linalg.norm(features_all_pre, ord=2, axis=1).reshape((features_all_pre.shape[0], 1))
     revop_map = get_roc_score_matrix(features_all_pre, Q.shape[1])
     print('inference: {}'.format(revop_map))
@@ -136,99 +130,54 @@ def gae_for(args, aa):
 
     def run_tf(EARLY_STOPPING_ITR = 40, alpha = 2, beta = 0.25):
         tf.reset_default_graph()
-        #with tf.device('/device:GPU:1'):
-        #adj_label_spt = convert_sparse_matrix_to_sparse_tensor(adj_label)
         featts_ph = tf.placeholder(dtype=tf.float32, shape=[None, 2048])
 
-        adj_norm_spt = convert_sparse_matrix_to_sparse_tensor(adj_norm)
-        adj_all_norm_spt = convert_sparse_matrix_to_sparse_tensor(adj_all_norm)
-        adj_ph = tf.sparse_placeholder(dtype=tf.float32, shape=[None, None])
+        regularizer = tf.contrib.layers.l2_regularizer(scale=1e-5)
 
-        #ipdb.set_trace()
-        regularizer = tf.contrib.layers.l2_regularizer(scale=1e-15)
-
-        #print(features_pre.shape[0])
         training_dataset = tf.data.Dataset.from_tensor_slices(featts_ph)
-        #training_dataset = training_dataset.shuffle(buffer_size=10000)
         training_dataset = training_dataset.batch(args.batch_size)
         training_dataset = training_dataset.repeat()
 
         validation_dataset = tf.data.Dataset.from_tensor_slices(featts_ph)
-        validation_dataset = validation_dataset.batch(5063)
+        validation_dataset = validation_dataset.batch(features_all.shape[0])
 
         iterator = tf.data.Iterator.from_structure(training_dataset.output_types,
                                                    training_dataset.output_shapes)
         itr_train_init_op = iterator.make_initializer(training_dataset)
-        #ipdb.set_trace()
         itr_valid_init_op = iterator.make_initializer(validation_dataset)
         next_element = iterator.get_next()
 
-        #ipdb.set_trace()
 
         # # 1st GCN
         with tf.variable_scope('GCN1'):
-            # np.random.seed(428)
-            init_w = (np.random.randn(args.hidden1, args.hidden1) * 1e-5)
-            # init_w = np.zeros((args.hidden1,args.hidden1))
+            init_w = (np.random.randn(args.hidden1, args.hidden1) * 1e-7)
             init_w[np.where(np.eye(args.hidden1) != 0)] = 1
-            # ipdb.set_trace()
-            # print(init_w)
             constant_init = tf.convert_to_tensor(init_w, dtype=tf.float32)
             W1 = tf.get_variable(name="w", dtype=tf.float32,
                                  initializer=constant_init,
                                  regularizer=regularizer)
             B1 = tf.get_variable(name='b', shape=[args.hidden1], dtype=tf.float32,
                                  initializer=tf.constant_initializer(0.0))
-            walk1 = tf.sparse_tensor_dense_matmul(adj_ph, next_element)
-            output1 = tf.nn.bias_add(tf.matmul(walk1, W1), B1)
+            output1 = tf.nn.bias_add(tf.matmul(next_element, W1), B1)
             output1 = tf.nn.elu(output1)
 
         with tf.variable_scope('GCN2'):
-            # np.random.seed(428)
-            init_w = (np.random.randn(args.hidden1, args.hidden1) * 1e-5)
-            # init_w = np.zeros((args.hidden1,args.hidden1))
+            init_w = (np.random.randn(args.hidden1, args.hidden1) * 1e-7)
             init_w[np.where(np.eye(args.hidden1) != 0)] = 1
-            # ipdb.set_trace()
-            # print(init_w)
             constant_init = tf.convert_to_tensor(init_w, dtype=tf.float32)
             W2 = tf.get_variable(name="w", dtype=tf.float32,
                                  initializer=constant_init,
                                  regularizer=regularizer)
             B2 = tf.get_variable(name='b', shape=[args.hidden1], dtype=tf.float32,
                                  initializer=tf.constant_initializer(0.0))
-            #walk2 = tf.sparse_tensor_dense_matmul(adj_ph, output1)
             output2 = tf.nn.bias_add(tf.matmul(output1, W2), B2)
             output2 = tf.nn.elu(output2)
 
-        # 1st GCN
-        # with tf.variable_scope('GCN1'):
-        #     W1 = tf.get_variable(name="w", shape=[feat_dim,args.hidden1], dtype=tf.float32,
-        #                          initializer=tf.random_normal_initializer(),
-        #                          regularizer=regularizer)
-        #     B1 = tf.get_variable(name='b', shape=[args.hidden1], dtype=tf.float32,
-        #                              initializer=tf.constant_initializer(0.0))
-        #     output1 = tf.nn.bias_add(tf.matmul(next_element, W1), B1)
-        #     output1 = tf.nn.elu(output1)
 
         with tf.variable_scope('InnerProduct'):
             hidden_emb = tf.nn.l2_normalize(output2, axis=1)
-            #hidden_emb = output1
             adj_preds = tf.matmul(hidden_emb, tf.transpose(hidden_emb))
             adj_preds = tf.nn.relu(adj_preds)
-            #adj_preds = tf.nn.dropout(adj_preds, 0.99)
-
-
-        # losses = tf.nn.weighted_cross_entropy_with_logits(
-        #     #tf.zeros_like(adj_preds, dtype=tf.float32),
-        #     adj_preds,
-        #     adj_preds,
-        #     pos_weight=1.0,
-        #     name='weighted_loss'
-        # )
-
-        #losses = tf.keras.backend.binary_crossentropy(adj_preds, adj_preds, False)
-
-        #losses = -1 * adj_preds ** 2 + 0.5 * adj_preds + 0.5
 
 
 
@@ -250,24 +199,15 @@ def gae_for(args, aa):
         stopping_tracker = 0
         session_conf = tf.ConfigProto(allow_soft_placement=True, log_device_placement=False)
         with tf.Session(config=session_conf) as sess:
-            #sess.run(itr_train_init_op, feed_dict={featts_ph: features})
-            #sess.run(iterator2.initializer)
+
 
             sess.run(train_init_op)
-            #ipdb.set_trace()
-            #hidden_emb_inf = sess.run(step2_norm)
-            #revop_map = get_roc_score_matrix(hidden_emb_inf, Q.shape[1])
-            #print("inference map:{}".format(revop_map))
-            #sess.run(train_init_op)
 
             itr = 0
             sess.run(itr_train_init_op, feed_dict={featts_ph: features_pre})
             while itr < args.epochs:
                 start_time = time.time()
-                #sess.run(global_step.assign(itr + 1))
-                #sess.run(tf.initialize_variables([labels]))
-                #ipdb.set_trace()
-                _, loss_out = sess.run([train_op, loss], feed_dict={adj_ph: adj_norm_spt})
+                _, loss_out = sess.run([train_op, loss])
                 end_time = time.time() - start_time
                # print(
                #     "itr: {}, train loss: {}, train time: {}".format(itr, loss_out, str(end_time)))
@@ -282,10 +222,9 @@ def gae_for(args, aa):
                     while True:
                         try:
                             if first_time_flag:
-                                hidden_emb_np = sess.run(hidden_emb, feed_dict={adj_ph: adj_all_norm_spt})
+                                hidden_emb_np = sess.run(hidden_emb)
                                 first_time_flag = False
                             else:
-                                break
                                 hidden_emb_np = np.concatenate([hidden_emb_np, sess.run(hidden_emb)], axis=0)
                         except tf.errors.OutOfRangeError:
                             break
@@ -302,20 +241,22 @@ def gae_for(args, aa):
                     sess.run(itr_train_init_op, feed_dict={featts_ph: features_pre})
             return best_revop
 
-    best_map = 0
-    best_alpha = 0
-    best_beta = 0
-    for  a in [0.01, 0.02, 0.05, 0.1, 0.2, 0.5, 1, 2, 5, 10, 20, 50, 100]:
-        for b in np.arange(0, 1, 0.05):
-            #ap = run_tf(alpha=a, beta=b)
-            ap = run_tf()
-            #ipdb.set_trace()
-            print("{:.4f} {:.4f} {:.4f}".format(a, b, ap))
-            if ap > best_map:
-                best_map = ap
-                best_alpha = a
-                best_beta = b
-    print("\n\nbest: {:.4f} {:.4f} {:.4f}".format(best_alpha, best_beta, best_map))
+    run_tf()
+
+    # best_map = 0
+    # best_alpha = 0
+    # best_beta = 0
+    # for  a in [1, 0.5, 2, 5]:
+    #     for b in np.arange(0.1, 0.8, 0.05):
+    #         ap = run_tf(alpha=a, beta=b)
+    #         #ap = run_tf()
+    #         #ipdb.set_trace()
+    #         print("{:.4f} {:.4f} {:.4f}".format(a, b, ap))
+    #         if ap > best_map:
+    #             best_map = ap
+    #             best_alpha = a
+    #             best_beta = b
+    # print("\n\nbest: {:.4f} {:.4f} {:.4f}".format(best_alpha, best_beta, best_map))
 
     
 
